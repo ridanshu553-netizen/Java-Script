af7530d7e2695e9b1a284b3d08d2ee3ef6f35191
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-07-22 23:06:09 +0530

    Add files via upload

---
+++ 01_Variables.js
@@ -0,0 +1,6 @@
+const accountId=1234
+let accountEmail="Ridanshu@googlegmail.com"
+var accountPassword="1234567"
+accountCity="Roorkee"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
