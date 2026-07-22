6afcd5b7e453e792156b5c0c03a1088af6dba9f8
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-07-22 23:38:08 +0530

    Delete 01_Variables.js

--- 01_Variables.js
+++
@@ -1,6 +0,0 @@
-const accountId=1234
-let accountEmail="Ridanshu@googlegmail.com"
-var accountPassword="1234567"
-accountCity="Roorkee"
-console.log(accountId)
-console.table([accountId,accountEmail,accountPassword,accountCity])
