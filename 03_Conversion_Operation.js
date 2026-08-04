5ac2f0effc6fcfeb5ad90226df1d674ad1cbed49
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-08-04 22:03:22 +0530

    Create 03_Conversion_Operation.js

---
+++ 03_Conversion_Operation.js
@@ -0,0 +1,52 @@
+let score="Ridanshu";
+// console.log(typeof score)
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let isLoggedIn=1
+let Bollean=Boolean(isLoggedIn)
+// console.log(Bollean)
+
+let someNumber=33
+
+let StringNumber=String(someNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// ************************Operations**********************
+
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="Hello"
+let str2=" Ridanshu"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+(2+"2")) 
+
+// Use the paranthesis brackets() properly to ensure correct answers in the operations
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
