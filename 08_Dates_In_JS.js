4897905c3765f259139a08443627f77482887f3b
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-08-12 22:11:01 +0530

    Implement date and timestamp examples in JS
    Add JavaScript code for date manipulation and timestamps.

---
+++ 08_Dates_In_JS.js
@@ -0,0 +1,33 @@
+//Dates
+
+let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString("en-IN"));
+// console.log(typeof myDate);
+
+// let myCreatedDate=new Date(2026,0,30)
+// let myCreatedDate=new Date(2026,0,30,7,48)
+// let myCreatedDate=new Date("2026-07-31")
+let myCreatedDate=new Date("07-31-2026")
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleString());
+
+//Time Stamps
+let myTimeStamp=Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate= new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+
+
+
