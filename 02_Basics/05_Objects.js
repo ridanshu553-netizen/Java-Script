fc95878502a4639f07520f3575ce6d06488c9bfe
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-08-12 22:07:52 +0530

    Implement object destructuring example
    Added an example of object destructuring in JavaScript.

---
+++ 02_Basics/05_Objects.js
@@ -0,0 +1,33 @@
+//Destructuring of the object
+
+const course={
+    coursename:"JavaScript",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+// console.log(course["price"]);
+
+//new syntax
+
+const {courseInstructor:CI}=course //Destructuring of the array
+console.log(CI);
+
+// {}-->Json
+
+// {
+//     "name":"Ridanshu",
+//     "coursename":"JavaScript",
+//     "price":"Free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+
+
+
+
