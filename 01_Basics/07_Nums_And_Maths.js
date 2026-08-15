91b35b3d2de77f55f89ffab384be3e4bccdb2707
ridanshu553-netizen <ridanshu553@gmail.com>
2026-08-15 22:21:53 +0530

    Add number and math examples in 07_Nums_And_Maths.js

---
+++ 01_Basics/07_Nums_And_Maths.js
@@ -0,0 +1,39 @@
+const score=100
+
+// const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(typeof balance)
+// console.log(balance.toFixed(3))
+
+const otherNumber=25.999999
+// console.log(otherNumber.toPrecision(3));// It returns the string value after the precision
+
+const hundreds=10000000
+// console.log(hundreds.toLocaleString("en-IN"))// It returns the values according to indian comma system for easy understanding of the codes such as e-commerce websites
+
+
+
+//++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+// console.log(Math)
+
+// console.log(Math.abs(-4))//abs--> It is the absolute value in the Math
+
+// console.log(Math.round(4.6))
+
+// console.log(Math.ceil(4.2))
+
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(4,3,6,8));
+
+// console.log(Math.max(4,3,6,8));
+
+// console.log(Math.random());
+
+ 
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
