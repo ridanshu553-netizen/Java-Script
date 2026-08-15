bc683db432345b35be0accf17b9d1762a3b69d9f
ridanshu553-netizen <ridanshu553@gmail.com>
2026-08-15 22:19:22 +0530

    Add control flow examples and conditional statements in Control_Flow-1.js

---
+++ 04_Control_Flow/Control_Flow-1.js
@@ -0,0 +1,64 @@
+//if
+
+// if(true){
+//     console.log("True");
+// }
+
+// if(false){
+//     console.log("False");    
+// }
+
+
+
+
+// const isUserLoggedIn=true;
+if(isUserLoggedIn){
+
+}
+
+if(2==="2"){  //=== triple equal strictly checks for value and data type we are comparing
+    console.log("Executed");
+}
+
+
+
+//<, >, <=, >=, ==, !=, !==, ==, ===
+
+const temperature=30;
+if(temperature===61){
+    // console.log("Less than 50");
+}
+else{
+    // console.log("Temperature is equal to 61");
+}
+// console.log("Temperature is greater than 50");
+
+const score=200;
+if(score>100){
+    const power="Fly";
+    // console.log(`The power of User is ${power}`);
+}
+
+const balance=1000;
+
+// if(balance>500) console.log("Run"),console.log("Run2");
+
+if(balance < 500){
+    // console.log("less Than 500");
+}
+else if(balance < 750){
+    // console.log("less Than 750");
+}
+else if(balance <900){
+    // console.log("less Than 750");
+}
+else{
+    // console.log("Less Than 1200");
+}
+
+const userLoggedIn=true;
+const debitCard=true;
+
+if(usserLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
